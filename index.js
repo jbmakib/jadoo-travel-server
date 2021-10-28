0b50cf7238293d60577a38a8f3559325b50cdd83
jbmakib <mdfoujolmobin@gmail.com>
2021-10-28 22:59:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("server started");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
