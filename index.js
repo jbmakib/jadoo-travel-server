0b3f4d01a3103f9ae9f86131a37715ff2b2c0874
jbmakib <mdfoujolmobin@gmail.com>
2021-10-28 23:22:10 +0600

    mongoclient, uri added

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express");
 require("dotenv").config();
+const { MongoClient } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdj6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("tourism");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("server started");
