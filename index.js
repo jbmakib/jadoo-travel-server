ea029848b97098d85af809591873744c3712912f
jbmakib <mdfoujolmobin@gmail.com>
2021-10-29 23:43:55 +0600

    added get and post api for service

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-require("dotenv").config();
 const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdj6v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -14,11 +18,23 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        app.get("/", (req, res) => {
-            res.send("db started");
-        });
 
         const database = client.db("tourism");
+        const servicesCollection = database.collection("services");
+
+        // GET API for services
+        app.get("/services", async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const result = await cursor.toArray();
+            res.json(result);
+        });
+
+        // POST API for services
+        app.post("/services", async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        });
     } finally {
         // await client.close();
     }
